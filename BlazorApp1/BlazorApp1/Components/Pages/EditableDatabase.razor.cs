91c108ecfc8bb78c829fb15dee16561b9cefa155
guswanger <gusswanger@gmail.com>
2024-02-29 09:07:04 -0500

    didn't fix it

--- BlazorApp1/BlazorApp1/Components/Pages/EditableDatabase.razor.cs
+++ BlazorApp1/BlazorApp1/Components/Pages/EditableDatabase.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorApp1.Components.Pages.Classes;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp1.Components.Pages;
 
@@ -7,4 +8,24 @@ public partial class EditableDatabase
 
     [Inject]
     private AppDbContext DbContext { get; set; }
+    /*public Part newPart = new();
+    public string NewName { get; set; }
+    public string NewQuantity { get; set; }
+    public decimal NewCost { get; set; }
+    public string NewVendor { get; set; }
+    public string NewLocation { get; set; }
+    protected override void OnInitialized()
+    {
+        newPart.Id = DbContext.Parts.Max(p => p.Id) + 1;
+    }
+    public async void SaveChanges()
+    {
+        newPart.Name = NewName;
+        newPart.Quantity = NewQuantity;
+        newPart.Cost = NewCost;
+        newPart.VendorID = NewVendor;
+        newPart.LocationID = NewLocation;
+        DbContext.Parts.Add(newPart);
+        await DbContext.SaveChangesAsync();
+    }*/
 }
