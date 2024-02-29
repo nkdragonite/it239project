c7739c1c182e9bb583ea68cf9a79a6cad2d955f8
jessicag09 <jessica.gravel555@gmail.com>
2024-02-28 21:35:25 -0500

    commit

--- BlazorApp1/BlazorApp1/Components/Pages/Classes/Part.cs
+++ BlazorApp1/BlazorApp1/Components/Pages/Classes/Part.cs
@@ -3,22 +3,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorApp1.Components.Pages.Classes
 {
-	[Table("Parts")]
-	public class Part
-	{
-		[Key, Required, Column("Part_Id")]
-		public string Id { get; set; }
-		//[Required, Column("Part_Name")]
-		//public string Name { get; set; }
-		//[Required, Column("Part_Desc")]
-		//public string Description { get; set; }
-		[Required, Column("Vendor_Id")]
-		public string VendorID { get; set; }
-		[Required, Column("Part_Quantity")]
-		public int Quantity { get; set; }
-		[Required, Column("Part_Cost")]
-		public decimal Cost { get; set; }
-		[Required, Column("Location_Id")]
-		public string LocationID { get; set; }
-	}
+    [Table("Parts")]
+    public class Part
+    {
+        [Key, Required, Column("Part_Id")]
+        public int Id { get; set; }
+		
+        [Column("Part_Name")]
+        public string Name { get; set; }
+	
+        [Column("Vendor_Id")]
+        public string VendorID { get; set; }
+		
+        [Column("Part_Quantity")]
+        public string Quantity { get; set; }
+		
+        [Column("Part_Cost")]
+        public decimal Cost { get; set; }
+		
+        [Column("Location_Id")]
+        public string LocationID { get; set; }
+    
+    }
 }
